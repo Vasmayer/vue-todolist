523811ceaccfdd5a0878dec43b52d619233bb6be
Vasmayer <lealimarco90@gmail.com>
2022-01-28 16:48:34 +0100

    interface html and css completed

--- js/script.js
+++ js/script.js
@@ -1,13 +1,45 @@
 /* 
-
+Descrizione:
+Rifare l'esercizio della to do list.
+Ogni todo sarà un oggetto, formato da due proprietà:
+- text, una stringa che indica il testo del todo
+- done, un booleano (true/false) che indica se il todo è stato fatto oppure no
+MILESTONE 1
+Stampare all'interno di una lista, un item per ogni todo.
+Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
+MILESTONE 2
+Visualizzare a fianco ad ogni item ha una "x": cliccando su di essa, il todo viene rimosso dalla lista.
+MILESTONE 3
+Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
+Bonus:
+1- oltre al click sul pulsante, intercettare anche il tasto ENTER per aggiungere il todo alla lista
+2- cliccando sul testo dell'item, invertire il valore della proprietà done del todo corrispondente (se done era uguale a false, impostare true e viceversa)
+Buon lavoro e buon weekend ragazzi!
  */
 
 Vue.config.devtools = true;
 
 var app5 = new Vue({
     el: '#root',
-    data: {
-     
+    data:{
+      todos:[
+       {
+            text:"Andare a fare la spesa",
+            done:false
+       },
+       {
+            text:"Andare al lavoro",
+            done:true
+       },
+       {
+            text:"Studiare i nuovi concetti",
+            done:false
+       },
+       {
+            text:"Uscire con gli amici",
+            done:false
+       }
+      ]
     },
     methods:
     {
